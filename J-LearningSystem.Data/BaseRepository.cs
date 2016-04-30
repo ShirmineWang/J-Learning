8cda23987e815728639b99af84bc2627458eda41
TangTing <tangtingsg@gmail.com>
2016-04-30 15:51:49 +0800

    first version

---
+++ J-LearningSystem.Data/BaseRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J_LearningSystem.Data
+{
+    public interface IRepository<T> where T : class {
+        void Add(T entity);
+        void Remove(T entity);
+        T GetById(string id);
+
+        IQueryable<T> GetAll();
+
+        void Save();
+    }
+
+    public class Repository<T> : IRepository<T> where T : class {
+        protected SystemContext _db = null;
+
+        public Repository(SystemContext db)
+        {
+            _db = db;
+        }
+        public void Add(T entity)
+        {
+            _db.Set<T>().Add(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _db.Set<T>().Remove(entity);
+        }
+
+        public T GetById(string id)
+        {
+            return _db.Set<T>().Find(id);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _db.Set<T>().AsQueryable();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+    }
+}
---
+++ J-LearningSystem.Data/CourseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using J_LearingSystem.Models;
+
+namespace J_LearningSystem.Data
+{
+    public class CourseRepository : Repository<Course>
+    {
+        public CourseRepository(SystemContext db) : base(db)
+        {
+
+        }
+    }
+}
